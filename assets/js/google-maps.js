9e67fb050ab4ada8f3bf082303869e68caa77ed0
Carlos Acevedo <caacevedo@unal.edu.co>
2024-04-03 22:03:55 -0500

    17 Act

--- assets/js/google-maps.js
+++ assets/js/google-maps.js
@@ -1,6 +1,6 @@
 function initMap() {
     // Latitude and Longitude
-    var myLatLng = {lat: -6.207690, lng: 106.985270};
+    var myLatLng = {lat: 6.230276257311738, lng: -75.6038952232153};
 
     var map = new google.maps.Map(document.getElementById('google-maps'), {
         zoom: 17,
@@ -10,6 +10,6 @@ function initMap() {
     var marker = new google.maps.Marker({
         position: myLatLng,
         map: map,
-        title: 'South Jakarta, INA' // Title Location
+        title: 'Medellin,colombia' // Title Location
     });
 }
