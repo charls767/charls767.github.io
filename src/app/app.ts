a62c6b81941974e040005463ea978ba3a0908d5e
charls767 <caacevedo@unal.edu.co>
2025-09-24 20:01:54 -0500

    Hidden Im - Proyects

--- src/app/app.ts
+++ src/app/app.ts
@@ -3,13 +3,13 @@ import { RouterOutlet } from '@angular/router';
 import {HeaderComponent} from './head/header/header'
 import { AboutMe } from './about-me/about-me';
 import { Tecnologias } from './tecnologias/tecnologias';
-import { Proyectos } from './proyectos/proyectos';
+/*import { Proyectos } from './proyectos/proyectos';*/
 import { Formacion } from './formacion/formacion';
 import { BotonScroll } from './boton-scroll/boton-scroll';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet,HeaderComponent,AboutMe,Tecnologias,Proyectos,Formacion,BotonScroll],
+  imports: [RouterOutlet,HeaderComponent,AboutMe,Tecnologias,Formacion,BotonScroll],
   templateUrl: './app.html',
   styleUrl: './app.css'
 })
